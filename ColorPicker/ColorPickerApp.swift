96e2fb0737cc61263fc433583677c3ddf7b8eec8
Stef Kors <stef@beamapp.co>
2023-02-14 15:16:13 +0100

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Stef Kors on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
